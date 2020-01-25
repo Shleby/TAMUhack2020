83b954535cebc47c0b01d69cefa64eb285fec1fc
Tim Bartee <watsurta@hotmail.com>
2020-01-25 17:11:46 -0600

    updating page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-eheader">
+          <h3>Important:if you or someone you know is in a life-threatening situation, call[location_based_number]</h3>
+        </div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React suckers change haruka</h2>
+          <h2>Company</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
