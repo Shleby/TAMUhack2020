9a321d329c7404ff2821c8fe13b8d3a2dbaeffc5
Tim Bartee <watsurta@hotmail.com>
2020-01-25 17:42:09 -0600

    freak off

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
           <h3>Important:if you or someone you know is in a life-threatening situation, call[location_based_number]</h3>
         </div>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+         
           <h2>Company</h2>
+          <h4> tagline here</h4>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
