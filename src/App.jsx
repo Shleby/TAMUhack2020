f383565676edb538d7d85c4f33e9be6ee412f1c6
Cole Chronowski <cole.chronowski@ou.edu>
2020-01-25 17:51:49 -0600

    fixed weird header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 
@@ -8,16 +8,12 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="background">
-      <Link to="/">
-        <h1 className="title">cers</h1>
-        <p className="subtitle">Centralized Emergency Relief System</p>
-      </Link>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
     );
   }
 }
