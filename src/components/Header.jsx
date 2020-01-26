6e8fb9848c98467abcb41e5cb902688a771ee97a
Harukainidekirukoto <haruka.wakamori@gmail.com>
2020-01-25 22:02:51 -0600

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../LOGO.svg";
 
-<<<<<<< HEAD
 import './Header.css';
+
 class Header extends React.Component {
     constructor(){
         super()
@@ -42,40 +42,6 @@ class Header extends React.Component {
             ]
         }
     }    
-    render() {
-        return(
-            <div className="header">
-                <img src={logo} className="logo" alt="logo" />
-                <h2>Next Steps</h2>
-                <Link to="/">
-                    <h2>Home</h2>
-                </Link>
-                <Link to="/about/">
-                    <h2>About Us</h2>
-                </Link>
-                <Link to="/resources/">
-                    <h2>Resources</h2>
-                </Link>
-                <Link to="/language/">
-                    <h2>Language</h2>
-                </Link>
-<div className="dd-wrapper">
-    <div className="dd-header">
-        <div className="dd-header-title"></div>
-    </div>
-    <ul className="dd-list">
-        <li className="dd-list-item"></li>
-        <li className="dd-list-item"></li>
-        <li className="dd-list-item"></li>
-    </ul>
-</div>
-            </div>
-        );
-    }
-=======
-import "./Header.css";
-
-class Header extends React.Component {
   render() {
     return (
       <div className="header">
@@ -91,13 +57,19 @@ class Header extends React.Component {
         <Link to="/resources/">
           <h2>Resources</h2>
         </Link>
-        <select>
-          <option value="EN">EN</option>
-        </select>
-      </div>
+        <div className="dd-wrapper">
+    <div className="dd-header">
+        <div className="dd-header-title"></div>
+    </div>
+    <ul className="dd-list">
+        <li className="dd-list-item"></li>
+        <li className="dd-list-item"></li>
+        <li className="dd-list-item"></li>
+    </ul>
+</div>
+            </div>
     );
   }
->>>>>>> c760b1730e0665819fd56501112b9914cb3b5438
 }
 
 export default Header;
