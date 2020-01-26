086d14f7f4c471b47951dd1a91e3ee581a638a8b
Shleby <shelby.a.huffy@gmail.com>
2020-01-25 21:56:15 -0600

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../LOGO.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../LOGO.svg";
 
-import './Header.css';
+import "./Header.css";
 
 class Header extends React.Component {
-    render() {
-        return(
-            <div className="header">
-                <img src={logo} className="logo" alt="logo" />
-                <h2>Next Steps</h2>
-                <Link to="/">
-                    <h2>Home</h2>
-                </Link>
-                <Link to="/about/">
-                    <h2>About Us</h2>
-                </Link>
-                <Link to="/resources/">
-                    <h2>Resources</h2>
-                </Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>Next Steps</h1>
+        <p>We're Here to Help</p>
+        <Link to="/">
+          <h2>Home</h2>
+        </Link>
+        <Link to="/about/">
+          <h2>About Us</h2>
+        </Link>
+        <Link to="/resources/">
+          <h2>Resources</h2>
+        </Link>
+        <select>
+          <option value="EN">EN</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,39 @@
-import React from 'react';
-import Header from './Header';
-import "./Home.css"
-import Popup from './Popup';
-
+import React from "react";
+import Header from "./Header";
+import "./Home.css";
+import Popup from "./Popup";
 
 class Home extends React.Component {
-    constructor(props){  
-  super(props);  
-  this.state = { showPopup: true };  
-  }  
+  constructor(props) {
+    super(props);
+    this.state = { showPopup: true };
+  }
 
-    togglePopup() {  
-        this.setState({  
-       showPopup: !this.state.showPopup  
-        });  
-   }  
-  
-    render() {
-        return(
-            <div>
-            <div>  
-               
-                {/* popup*/}
-                   
-            
-                {this.state.showPopup ?  
-            <Popup  
-                      text='Click "Close Button" to hide popup'  
-                      closePopup={this.togglePopup.bind(this)}  
-            />  
-            : null  
-            }  
-            </div>  
-             <div className="Home">
-                <Header/>
-            </div>
-            </div>
-        )
-    }
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {/* popup*/}
+
+          {this.state.showPopup ? (
+            <Popup
+              text='Click "Close Button" to hide popup'
+              closePopup={this.togglePopup.bind(this)}
+            />
+          ) : null}
+        </div>
+        <div className="Home">
+          <Header />
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
 
 export default Home;
