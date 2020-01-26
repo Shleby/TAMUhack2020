70a99c248267e1d2c3a2002b77a79a5219b632da
Cole Chronowski <cole.chronowski@ou.edu>
2020-01-25 21:13:19 -0600

    Fixed formatting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,35 +6,32 @@ import Popup from './Popup';
 
 class Home extends React.Component {
     constructor(props){  
-  super(props);  
-  this.state = { showPopup: true };  
-  }  
+        super(props);  
+        this.state = { showPopup: true };  
+    }  
 
     togglePopup() {  
         this.setState({  
-       showPopup: !this.state.showPopup  
+            showPopup: !this.state.showPopup  
         });  
-   }  
+    }  
   
     render() {
         return(
             <div>
-            <div>  
-               
-                {/* popup*/}
-                   
-            
-                {this.state.showPopup ?  
-            <Popup  
-                      text='Click "Close Button" to hide popup'  
-                      closePopup={this.togglePopup.bind(this)}  
-            />  
-            : null  
-            }  
-            </div>  
-             <div className="Home">
-                <Header/>
-            </div>
+                <div>  
+                    {/* popup*/}
+                    {this.state.showPopup ?  
+                        <Popup  
+                                text='Click "Close Button" to hide popup'  
+                                closePopup={this.togglePopup.bind(this)}  
+                        />  
+                        : null  
+                    }  
+                </div>  
+                <div className="Home">
+                    <Header/>
+                </div>
             </div>
         )
     }
